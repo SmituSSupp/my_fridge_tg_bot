07f5139ca4ec3e82530c573af117b0bb8836c725
SmituS <mixercool1@gmail.com>
2022-01-23 22:09:48 +0300

    [f] {kitchen} {main} add Fridge and Ingredient classed for futher interaction

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 # This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def main() -> None:
+    """Run the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater("")
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    fridge_handler = ConversationHandler(
+        entry_points=[CommandHandler('existing_fridge', existing_fridge)],
+        states={
+            FOOD_EXPORT_IMPORT: [MessageHandler(Filters.text & ~Filters.command, callback=update_food)],
+        },
+        fallbacks=[CommandHandler('close_fridge', close_fridge)],
+    )
+
+    dispatcher.add_handler(fridge_handler)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
